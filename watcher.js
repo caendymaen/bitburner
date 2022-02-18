5f48b9133b6836eae98e9971cbaf5f0549aee611
caendymaen <99954424+caendymaen@users.noreply.github.com>
2022-02-18 21:24:48 +0100

    Update watcher.js

--- watcher.js
+++ watcher.js
@@ -1,4 +1,4 @@
-import {DeployableScripts, TaskPort, TargetPort, ServerList} from "utils.js";
+import {GeneralMultipliers, DeployableScripts, TaskPort, TargetPort, ServerList} from "utils.js";
 /** @param {NS} ns **/
 export async function main(ns) {
 	//define a general delay for looping/watching
@@ -6,10 +6,41 @@ export async function main(ns) {
 	//clear ports that are important for watcher<>bot communication
 	await ns.clearPort(TaskPort);
 	await ns.clearPort(TargetPort);
+	//create the list of servers
+	let servers = new ServerList(ns);
+	//run through the list of servers
+	for(let i = 0; i < servers.serverList.length; i++) {
+		//get the current server
+		let server = servers.serverList[i];
+		//if there is root access to the server and it has more than 0 gb ram
+		if(server.rootaccess && server.maxram > 0 && server.servername != "home") {
+			//check, if deployable scripts are already on the server, if not copy them
+			for(let scriptname of DeployableScripts) {
+				//if the deployable scripts do not exist, copy them
+				if(!ns.fileExists(scriptname, server.servername)) {
+					await ns.scp(scriptname, server.servername);
+				}
+				else {
+					//if the deployable scripts are not running at the moment 
+					//remove them and copy them again (in case there were updates done on the home server)
+					if(!ns.scriptRunning(scriptname, server.servername)) {
+						ns.rm(scriptname, server.servername);
+						await ns.scp(scriptname, server.servername);
+					}
+				}
+			}
+			//check, if bot.js is not running on the server AND the server's max ram
+			//let it run bot.js and any additional script (weaken.js, grow.js, hack.js)
+			if(!ns.scriptRunning("bot.js", server.servername) && (server.maxram > ((ns.getScriptRam("bot.js", server.servername) + ns.getScriptRam("weaken.js", server.servername))))) {
+				//run bot.js on the server
+				ns.exec("bot.js", server.servername, 1, (watchdelay * 2));
+			}
+		}
+	}
 	//loop infinitely
 	while(true) {
 		//create the list of servers
-		let servers = new ServerList(ns);
+		servers = new ServerList(ns);
 		//create a list of servers, which have minimum security level and maximum money
 		let hackableservers = [];
 		//run through the list of servers
@@ -25,12 +56,6 @@ export async function main(ns) {
 						await ns.scp(scriptname, server.servername);
 					}
 					else {
-						//if the deployable scripts are not running at the moment 
-						//remove them and copy them again (in case there were updates done on the home server)
-						if(!ns.scriptRunning(scriptname, server.servername)) {
-							ns.rm(scriptname, server.servername);
-							await ns.scp(scriptname, server.servername);
-						}
 					}
 				}
 				//check, if bot.js is not running on the server AND the server's max ram
@@ -41,9 +66,9 @@ export async function main(ns) {
 				}
 			}
 			//if there is root access to the server and it has more than 0 gb ram and sever is hackable create the strategy
-			if(server.rootaccess && server.ishackable && server.maxram > 0 && server.servername != "home") {
+			if(server.rootaccess && server.ishackable && server.servername != "home") {
 				//if the server's security level is equal or bigger than its base security level, try to queue
-				if(server.securitylevel > ns.getServerMinSecurityLevel(server.servername)) {
+				if(server.securitylevel > (ns.getServerMinSecurityLevel(server.servername) * GeneralMultipliers.minServerSecurity)) {
 					while(!await ns.tryWritePort(TaskPort, "weaken")) {
 						await ns.asleep(watchdelay);
 					}
@@ -52,7 +77,7 @@ export async function main(ns) {
 					}
 				}
 				//if min security level was reached and max money not, grow
-				else if(ns.getServerMoneyAvailable(server.servername) < ns.getServerMaxMoney(server.servername)) {
+				else if(ns.getServerMoneyAvailable(server.servername) < (ns.getServerMaxMoney(server.servername) * GeneralMultipliers.maxServerMoney)) {
 					while(!await ns.tryWritePort(TaskPort, "grow")) {
 						await ns.asleep(watchdelay);
 					}
@@ -95,7 +120,7 @@ export async function main(ns) {
 				//if there is root access to the server and it has more than 0 gb ram
 				if(server.rootaccess && server.ishackable && server.maxram > 0 && server.servername != "home") {
 					//if minimum security level is not reached, weaken
-					if(server.securitylevel > ns.getServerMinSecurityLevel(server.servername)) {
+					if(server.securitylevel > (ns.getServerMinSecurityLevel(server.servername) * GeneralMultipliers.minServerSecurity)) {
 						while(!await ns.tryWritePort(TaskPort, "weaken")) {
 							await ns.asleep(watchdelay);
 						}
