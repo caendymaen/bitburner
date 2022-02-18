3e2f454b5b2be38f09f42b0f2063530adecb6c1e
caendymaen <99954424+caendymaen@users.noreply.github.com>
2022-02-18 23:15:31 +0100

    Create const.js

---
+++ const.js
@@ -0,0 +1,10 @@
+//define a constant for general multipliers
+export const GeneralMultipliers = {
+	maxServerMoney: 1,
+	minServerSecurity: 1
+};
+//define constants for port handling
+export const TaskPort = 1;
+export const TargetPort = 2;
+//define a constant for scripts to deploy on servers
+export const DeployableScripts = ["weaken.js", "grow.js", "hack.js", "bot.js", "const.js"];
