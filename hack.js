5f08e22b80b62e2247ec8b2b2712a5cf6eff440e
caendymaen <99954424+caendymaen@users.noreply.github.com>
2022-02-21 06:22:03 +0100

    Update hack.js

--- hack.js
+++ hack.js
@@ -1,5 +1,10 @@
 /** @param {NS} ns **/
 export async function main(ns) {
 	let target = ns.args[0] || ns.getHostname();
+	let delay = ns.args[1] || 0;
+	let id = ns.args[2] || "";
+	if(delay > 0) {
+		await ns.asleep(delay);
+	}
 	await ns.hack(target);
 }
