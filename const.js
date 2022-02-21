ebadf15e4adb856025d1f7e5eaf89df5368ca686
caendymaen <99954424+caendymaen@users.noreply.github.com>
2022-02-21 06:21:17 +0100

    Update const.js

--- const.js
+++ const.js
@@ -4,13 +4,14 @@ export const GeneralMultipliers = {
 	minServerSecurity: 1
 };
 //define constants for port handling
-export const TaskPort = [1, 2, 3, 4, 5];
-export const TargetPort = [6, 7, 8, 9, 10];
+export const BotPorts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 //define a constant for scripts to deploy on servers
 export const DeployableScripts = ["weaken.js", "grow.js", "hack.js", "bot.js", "const.js"];
+export const BotScript = "bot.js";
+export const AttackMaxRAM = 1.8;
 //define a constant for delays
 export const GeneralDelay = 25;
 //define a constant for the watcher<>bot delay multiplier
 export const BotDelayMultiplier = 2;
 //define a constant for maximum concurrent hacks from a bot for 1 target
-export const BotMaxHacksPerTarget = 10;
+export const BotMaxHacksPerTarget = 5;
